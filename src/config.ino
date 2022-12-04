c19eb3526918bf8bd3293b791a5b3b08af4910fb
Dirk Heilig <heilig@adesso-mobile.de>
2022-12-04 16:32:04 +0100

    pin mapping for PCB

--- src/config.ino
+++ src/config.ino
@@ -3,17 +3,17 @@
 
 
 #define PIN_LED 13
-#define PIN_CLOCK 9
+#define PIN_CLOCK 11
 #define PIN_LATCH 10
-#define PIN_DATA 11
+#define PIN_DATA 9
 
 #define PIN_UP 8
-#define PIN_DOWN 7
-#define PIN_LEFT 6
-#define PIN_RIGHT 5
-#define PIN_FIRE 3
-#define PIN_FIRE2 2
-#define PIN_FIRE3 4
+#define PIN_DOWN 6
+#define PIN_LEFT 5
+#define PIN_RIGHT 2
+#define PIN_FIRE 7
+#define PIN_FIRE2 4
+#define PIN_FIRE3 3
 
 #define MIN_AUTO_FIRE_DELAY 2
 #define MAX_AUTO_FIRE_DELAY 64
